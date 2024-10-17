17786e4f9d3cc8af24116fe53fda288a5a3ee078
SmirnovM <misha-smirnov@inbox.ru>
2024-10-17 17:08:37 +0300

    Add lab4 solution

--- cvlib/src/corner_detector.cpp
+++ cvlib/src/corner_detector.cpp
@@ -10,6 +10,7 @@
 
 namespace cvlib
 {
+
 // static
 cv::Ptr<corner_detector_fast> corner_detector_fast::create()
 {
@@ -18,8 +19,72 @@ cv::Ptr<corner_detector_fast> corner_detector_fast::create()
 
 void corner_detector_fast::detect(cv::InputArray image, CV_OUT std::vector<cv::KeyPoint>& keypoints, cv::InputArray /*mask = cv::noArray()*/)
 {
+    
+    cv::Mat image_mat = image.getMat();
+    if (image_mat.channels() > 1)
+		cv::cvtColor(image, image_mat, cv::COLOR_BGR2GRAY);
+
+    int t = 10;
+    int N_t = 12;
     keypoints.clear();
-    // \todo implement FAST with minimal LOCs(lines of code), but keep code readable.
+
+    int rows = image_mat.rows;
+    int cols = image_mat.cols;
+    
+	for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            int i_p = image_mat.at<unsigned char>(i, j);
+            int n = 0;
+
+            // neighbour pixels
+            std::pair<int, int> circle[16];
+            circle[0] = std::pair<int, int>(std::max(i - 3, 0), j);
+            circle[1] = std::pair<int, int>(std::max(i - 3, 0), std::min(j + 1, cols-1));
+            circle[2] = std::pair<int, int>(std::max(i - 2, 0), std::min(j + 2, cols-1));
+            circle[3] = std::pair<int, int>(std::max(i - 1, 0), std::min(j + 3, cols-1));
+            circle[4] = std::pair<int, int>(i, std::min(j + 3, cols-1));
+            circle[5] = std::pair<int, int>(std::min(i + 1, rows-1), std::min(j + 3, cols-1));
+            circle[6] = std::pair<int, int>(std::min(i + 2, rows-1), std::min(j + 2, cols-1));
+            circle[7] = std::pair<int, int>(std::min(i + 3, rows-1), std::min(j + 1, cols-1));
+            circle[8] = std::pair<int, int>(std::min(i + 3, rows-1), j);
+            circle[9] = std::pair<int, int>(std::min(i + 3, rows-1), std::max(j - 1, 0));
+            circle[10] = std::pair<int, int>(std::min(i + 2, rows-1), std::max(j - 2, 0));
+            circle[11] = std::pair<int, int>(std::min(i + 1, rows-1), std::max(j - 3, 0));
+            circle[12] = std::pair<int, int>(i, std::max(j - 3, 0));
+            circle[13] = std::pair<int, int>(std::max(i - 1, 0), std::max(j - 3, 0));
+            circle[14] = std::pair<int, int>(std::max(i - 2, 0), std::max(j - 2, 0));
+            circle[15] = std::pair<int, int>(std::max(i - 3, 0), std::max(j - 1, 0));
+
+            // check pixels on horizontal and vertical directions
+            for (int k = 0; k < 16; k += 4)
+                if (abs(image_mat.at<unsigned char>(circle[k].first, circle[k].second) - i_p) > t)
+                    n++;
+
+            if (n >= 3)
+            {
+                
+                // check all pixels
+                int N = 0;
+                for (int k = 0; k < 16; k++)
+                {
+                    if (abs(image_mat.at<unsigned char>(circle[k].first, circle[k].second) - i_p) > t)
+                        N++;
+                    else
+                        N = 0;
+
+					if (N == N_t || ((16-k) + N) < N_t)
+                        break;
+                }
+
+                // save key point
+                if (N >= N_t)
+                    keypoints.push_back(cv::KeyPoint((float)j, (float)i, 1));
+            }
+        }
+    }
+    
 }
 
 void corner_detector_fast::compute(cv::InputArray, std::vector<cv::KeyPoint>& keypoints, cv::OutputArray descriptors)
@@ -43,7 +108,7 @@ void corner_detector_fast::compute(cv::InputArray, std::vector<cv::KeyPoint>& ke
 }
 
 void corner_detector_fast::detectAndCompute(cv::InputArray, cv::InputArray, std::vector<cv::KeyPoint>&, cv::OutputArray descriptors, bool /*= false*/)
-{
-    // \todo implement me
-}
+    {
+        // \todo implement me
+    }
 } // namespace cvlib
--- cvlib/tests/corner_detector.cpp
+++ cvlib/tests/corner_detector.cpp
@@ -13,13 +13,121 @@ using namespace cvlib;
 TEST_CASE("simple check", "[corner_detector_fast]")
 {
     auto fast = corner_detector_fast::create();
-    cv::Mat image(10, 10, CV_8UC1);
-    SECTION("empty image")
+    
+    SECTION("flat image")
     {
+        cv::Mat image(10, 10, CV_8UC1, cv::Scalar{127});
         std::vector<cv::KeyPoint> out;
         fast->detect(image, out);
         REQUIRE(out.empty());
     }
 
-    // \todo add 5 or more tests (SECTIONs)
+    SECTION("edge image")
+    {
+        cv::Mat image_left(10, 5, CV_8UC1, cv::Scalar{50});
+        cv::Mat image_right(10, 5, CV_8UC1, cv::Scalar{80});
+        cv::Mat image_edge;
+        cv::hconcat(image_left, image_right, image_edge);
+        
+		std::vector<cv::KeyPoint> out;
+        fast->detect(image_edge, out);
+        REQUIRE(out.empty());
+    }
+
+	SECTION("flat L-corner image")
+    {
+        cv::Mat image_lt(5, 5, CV_8UC1, cv::Scalar{50});
+        cv::Mat image_rt(5, 5, CV_8UC1, cv::Scalar{120});
+        cv::Mat image_lb(5, 5, CV_8UC1, cv::Scalar{120});
+        cv::Mat image_rb(5, 5, CV_8UC1, cv::Scalar{120});
+        
+        cv::Mat image_top;
+        cv::Mat image_bottom;
+        cv::Mat image_L;
+        cv::hconcat(image_lt, image_rt, image_top);
+        cv::hconcat(image_lb, image_rb, image_bottom);
+        cv::vconcat(image_top, image_bottom, image_L);
+
+        std::vector<cv::KeyPoint> out;
+        fast->detect(image_L, out);
+        REQUIRE(out.size() == 0);
+    }
+
+	SECTION("L-corner image")
+    {
+        cv::Mat image_lt(5, 5, CV_8UC1, cv::Scalar{50});
+        cv::Mat image_rt(5, 5, CV_8UC1, cv::Scalar{120});
+        cv::Mat image_lb(5, 5, CV_8UC1, cv::Scalar{120});
+        cv::Mat image_rb(5, 5, CV_8UC1, cv::Scalar{120});
+        image_lt.at<unsigned char>(4, 4) = 90;
+
+		cv::Mat image_top;
+        cv::Mat image_bottom;
+        cv::Mat image_L;
+		cv::hconcat(image_lt, image_rt, image_top);
+        cv::hconcat(image_lb, image_rb, image_bottom);
+        cv::vconcat(image_top, image_bottom, image_L);
+
+        std::vector<cv::KeyPoint> out;
+        fast->detect(image_L, out);
+        REQUIRE(out.size() == 1);
+        REQUIRE(out[0].pt.x == 4);
+		REQUIRE(out[0].pt.y == 4);
+    }
+
+	SECTION("2 L-corner image")
+    {
+        cv::Mat image_lt(10, 10, CV_8UC1, cv::Scalar{50});
+        cv::Mat image_rt(10, 10, CV_8UC1, cv::Scalar{120});
+        cv::Mat image_lb(10, 10, CV_8UC1, cv::Scalar{120});
+        cv::Mat image_rb(10, 10, CV_8UC1, cv::Scalar{120});
+        image_lt.at<unsigned char>(9, 9) = 90;
+
+        cv::Mat image_top;
+        cv::Mat image_bottom;
+        cv::Mat image_L1;
+        cv::hconcat(image_lt, image_rt, image_top);
+        cv::hconcat(image_lb, image_rb, image_bottom);
+        cv::vconcat(image_top, image_bottom, image_L1);
+
+		cv::Mat image_L2;
+        cv::hconcat(image_L1, image_L1, image_L2);
+        
+        std::vector<cv::KeyPoint> out;
+        fast->detect(image_L2, out);
+
+        REQUIRE(out.size() == 2);
+        REQUIRE(out[0].pt.x == 9);
+        REQUIRE(out[0].pt.y == 9);
+        REQUIRE(out[1].pt.x == 29);
+        REQUIRE(out[1].pt.y == 9);
+
+    }
+
+	SECTION("T-corner image")
+    {
+        cv::Mat image_lt(10, 10, CV_8UC1, cv::Scalar{120});
+        cv::Mat image_rt(10, 10, CV_8UC1, cv::Scalar{120});
+        cv::Mat image_lb(10, 10, CV_8UC1, cv::Scalar{50});
+        cv::Mat image_rb(10, 10, CV_8UC1, cv::Scalar{90});
+        image_lb.at<unsigned char>(0, 9) = 70;
+        image_rb.at<unsigned char>(0, 0) = 70;
+
+        cv::Mat image_top;
+        cv::Mat image_bottom;
+        cv::Mat image_T;
+        cv::hconcat(image_lt, image_rt, image_top);
+        cv::hconcat(image_lb, image_rb, image_bottom);
+        cv::vconcat(image_top, image_bottom, image_T);
+
+        std::vector<cv::KeyPoint> out;
+        fast->detect(image_T, out);
+
+        REQUIRE(out.size() == 2);
+        REQUIRE(out[0].pt.x == 9);
+        REQUIRE(out[0].pt.y == 10);
+        REQUIRE(out[1].pt.x == 10);
+        REQUIRE(out[1].pt.y == 10);
+    }
+
 }
