85fe14222003dde98955b90afe51e3c56de8a134
SmirnovM <misha-smirnov@inbox.ru>
2024-10-18 12:41:46 +0300

    Add counterclockwise check of pixels

--- cvlib/src/corner_detector.cpp
+++ cvlib/src/corner_detector.cpp
@@ -37,47 +37,60 @@ void corner_detector_fast::detect(cv::InputArray image, CV_OUT std::vector<cv::K
         {
             int i_p = image_mat.at<unsigned char>(i, j);
             int n = 0;
-
-            // neighbour pixels
-            std::pair<int, int> circle[16];
-            circle[0] = std::pair<int, int>(std::max(i - 3, 0), j);
-            circle[1] = std::pair<int, int>(std::max(i - 3, 0), std::min(j + 1, cols-1));
-            circle[2] = std::pair<int, int>(std::max(i - 2, 0), std::min(j + 2, cols-1));
-            circle[3] = std::pair<int, int>(std::max(i - 1, 0), std::min(j + 3, cols-1));
-            circle[4] = std::pair<int, int>(i, std::min(j + 3, cols-1));
-            circle[5] = std::pair<int, int>(std::min(i + 1, rows-1), std::min(j + 3, cols-1));
-            circle[6] = std::pair<int, int>(std::min(i + 2, rows-1), std::min(j + 2, cols-1));
-            circle[7] = std::pair<int, int>(std::min(i + 3, rows-1), std::min(j + 1, cols-1));
-            circle[8] = std::pair<int, int>(std::min(i + 3, rows-1), j);
-            circle[9] = std::pair<int, int>(std::min(i + 3, rows-1), std::max(j - 1, 0));
-            circle[10] = std::pair<int, int>(std::min(i + 2, rows-1), std::max(j - 2, 0));
-            circle[11] = std::pair<int, int>(std::min(i + 1, rows-1), std::max(j - 3, 0));
-            circle[12] = std::pair<int, int>(i, std::max(j - 3, 0));
-            circle[13] = std::pair<int, int>(std::max(i - 1, 0), std::max(j - 3, 0));
-            circle[14] = std::pair<int, int>(std::max(i - 2, 0), std::max(j - 2, 0));
-            circle[15] = std::pair<int, int>(std::max(i - 3, 0), std::max(j - 1, 0));
-
+			 
+            // neighbour pixels on horizontal and vertical directions
+            bool circle[16];
+            circle[0]  = abs(image_mat.at<unsigned char>(std::max(i - 3, 0), j) - i_p) > t;
+            circle[4]  = abs(image_mat.at<unsigned char>(i, std::min(j + 3, cols-1)) - i_p) > t;
+            circle[8]  = abs(image_mat.at<unsigned char>(std::min(i + 3, rows-1), j) - i_p) > t;
+            circle[12] = abs(image_mat.at<unsigned char>(i, std::max(j - 3, 0)) - i_p) > t;
+            
             // check pixels on horizontal and vertical directions
             for (int k = 0; k < 16; k += 4)
-                if (abs(image_mat.at<unsigned char>(circle[k].first, circle[k].second) - i_p) > t)
-                    n++;
+				n += circle[k];
 
             if (n >= 3)
             {
+				// all neighbour pixel
+                circle[1]  = abs(image_mat.at<unsigned char>(std::max(i - 3, 0), std::min(j + 1, cols - 1)) - i_p) > t;
+                circle[2]  = abs(image_mat.at<unsigned char>(std::max(i - 2, 0), std::min(j + 2, cols - 1)) - i_p) > t;
+                circle[3]  = abs(image_mat.at<unsigned char>(std::max(i - 1, 0), std::min(j + 3, cols - 1)) - i_p) > t;
+                circle[5]  = abs(image_mat.at<unsigned char>(std::min(i + 1, rows - 1), std::min(j + 3, cols - 1)) - i_p) > t;
+                circle[6]  = abs(image_mat.at<unsigned char>(std::min(i + 2, rows - 1), std::min(j + 2, cols - 1)) - i_p) > t;
+                circle[7]  = abs(image_mat.at<unsigned char>(std::min(i + 3, rows - 1), std::min(j + 1, cols - 1)) - i_p) > t;
+                circle[9]  = abs(image_mat.at<unsigned char>(std::min(i + 3, rows - 1), std::max(j - 1, 0)) - i_p) > t;
+                circle[10] = abs(image_mat.at<unsigned char>(std::min(i + 2, rows - 1), std::max(j - 2, 0)) - i_p) > t;
+                circle[11] = abs(image_mat.at<unsigned char>(std::min(i + 1, rows - 1), std::max(j - 3, 0)) - i_p) > t;
+                circle[13] = abs(image_mat.at<unsigned char>(std::max(i - 1, 0), std::max(j - 3, 0)) - i_p) > t;
+                circle[14] = abs(image_mat.at<unsigned char>(std::max(i - 2, 0), std::max(j - 2, 0)) - i_p) > t;
+                circle[15] = abs(image_mat.at<unsigned char>(std::max(i - 3, 0), std::max(j - 1, 0)) - i_p) > t;
                 
                 // check all pixels
                 int N = 0;
-                for (int k = 0; k < 16; k++)
-                {
-                    if (abs(image_mat.at<unsigned char>(circle[k].first, circle[k].second) - i_p) > t)
+                int k;
+                int k_max = 16;
+				
+				// if 16 and 0 are pixels that satisfy condition then we should check if there is a sequence of N pixels in counterclockwise directions
+				if (circle[0])
+				{
+                    k = 15;
+                    while (circle[k] && k >= 0)
+                    {
                         N++;
-                    else
-                        N = 0;
-
-					if (N == N_t || ((16-k) + N) < N_t)
+                        k--;
+					}
+                    k_max = k+1;
+				}
+				
+				// check pixels in clockwise direction
+				for (k = 0; k < k_max; k++)
+				{
+					// if circle[k] == 0 then N = 0 else N = N + 1
+                    N = circle[k] * N + circle[k];
+                    if (N == N_t || ((k_max - k) + N) < N_t)
                         break;
-                }
-
+				}
+                 
                 // save key point
                 if (N >= N_t)
                     keypoints.push_back(cv::KeyPoint((float)j, (float)i, 1));
